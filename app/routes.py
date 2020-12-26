e0203fa08f1b8935784d687ce8809182f9263fe9
Harshana Mapalgama <harshana8979@gmail.com>
2020-12-26 23:57:55 +0530

    try to set routes.py

--- app/routes.py
+++ app/routes.py
@@ -8,23 +8,5 @@ def login():
         flash('Login requested for user {}, remeber_me={}'.format(form.username.data, form.remember_me.data))
         return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)   
-@app.route('/index')
-def index():
-    user = {'username': 'Miguel'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
 
 
-@app.route('/login')
-def login():
-    form = LoginForm()
-    return render_template('login.html', title='Sign In', form=form)
